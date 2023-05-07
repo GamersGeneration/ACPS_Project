a9cf55b4d77f2f9c955a8abe016b177a74fc066e
Parixit Raj Chaudhary <48518511+GamersGeneration@users.noreply.github.com>
2023-05-07 19:34:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,13 +8,26 @@
 # Copyright:   (c) rePUBLIC 2023
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
-from flask import redirect,url_for
+from flask import redirect,url_for, send_from_directory
+import os
+import pickle
 from flask import Flask, render_template
 import plotly.offline as pyo
 import plotly.graph_objs as go
-app = Flask(__name__)
+UPLOAD_FOLDER = "C:/Users/rePUBLIC/Desktop/test"
+app = Flask(__name__, template_folder='templates', static_folder='static')
 import io
 import random
+import joblib
+import tensorflow
+from flask import Flask, render_template, request
+import classifier
+
+from werkzeug.utils import secure_filename
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#import cv2
+import numpy as np
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import base64
 from PIL import Image
@@ -23,34 +36,32 @@ from flask import Flask, render_template, request, jsonify
 import plotly.offline as pyo
 import plotly.graph_objs as go
 from get_wallpapers import get_wallpapers
+from classifier import getPrediction
 #model = joblib.load('path/to/saved/model.pkl')
 @app.route('/')
+@app.route('/home')
 def home():
     #return 'Hello, World!'
     data = [1, 2, 3, 4, 5]
     plot_div = pyo.plot(create_plot(), output_type='div')
-    if 'logged_in' in session and session['logged_in']:
-        return render_template('index.html',data=data, plot_div=plot_div)
-    else:
-        return redirect(url_for('login'))
+    #if 'logged_in' in session and session['logged_in']:
+    return render_template('index.html')
+    #else:
+        #return redirect(url_for('login'))
 app.secret_key = 'lifeisgood'
 
+
+
+
+# Loading the ML model into Flask
+#model = load_model('model.h5')
+
+
+
+
 # Define a route to handle predictions
-#@app.route('/predict', methods=['POST'])
-def predict():
-    # Get the image file from the request
-    image_file = request.files['image']
-    # Read the image file into a PIL Image object
-    image = Image.open(io.BytesIO(image_file.read()))
-    # Preprocess the image
-    image_array = preprocess_image(image)
-    # Use the model to make a prediction
-    prediction = model.predict(image_array)[0]
-    # Convert the prediction to a string
-    prediction_str = 'Cow' if prediction == 1 else 'Not Cow'
-    # Return the prediction as a JSON object
-    return jsonify({'prediction': prediction_str})
-from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
+
+"""from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
 # Create instance of LoginManager
 login_manager = LoginManager(app)
 
@@ -82,11 +93,10 @@ def login():
             return redirect(url_for('home'))
         else:
             flash('Invalid username or password')
-    image_urls = get_wallpapers()
+
 
     # Choose a random image URL from the list
-    random_wallpaper = image_urls[random.randint(0, len(image_urls) - 1)]
-    return render_template('login.html', wallpaper=random_wallpaper)
+    return render_template('login.html')
 
 # Add a logout route to log out users
 @app.route('/logout')
@@ -101,29 +111,40 @@ def current_user():
     if current_user.is_authenticated:
         return jsonify({'username': current_user.id})
     else:
-        return jsonify({'username': 'anonymous'})
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image uploaded'})
-    image = request.files['image'].read()
-    prediction = predict_cow(image)
-    return jsonify({'prediction': prediction})
+        return jsonify({'username': 'anonymous'})"""
+@app.route('/predict', methods=['GET', 'POST'])
+def submit_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            getPrediction(filename)
+            label, acc = getPrediction(filename)
+            flash(label)
+            flash(acc)
+            flash(filename)
+            return render_template('predict.html')
+@app.route('/upload')
+def upload():
+    return render_template('upload.htm')
 def create_plot():
     trace = go.Bar(x=['Mon', 'Tue', 'Wed'], y=[1, 2, 3])
     layout = go.Layout(title='Buzzer Frequency')
     fig = go.Figure(data=[trace], layout=layout)
     return fig
-@app.route('/classify', methods=['POST'])
-def classify():
-    # Get the image data from the form
-    image = request.files['image'].read()
-
-    # Use the cow detector to predict whether the image contains a cow
-    prediction = cow_detector.predict_cow(image)
-
-    # Render the prediction template with the prediction result
-    return render_template('prediction.html', prediction=prediction)
+#@app.route('/course')
+#@app.route('/about')
+#@app.route('/contact')
+#@app.route('/blog')
+#def serve_static():
+#    root_dir = os.path.dirname(os.getcwd())
+#    return send_from_directory(os.path.join(root_dir, 'static'), 'course.html')
 if __name__ == '__main__':
     app.run(debug=True)
